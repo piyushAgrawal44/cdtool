e2739367fbe55617d4c3f95bcc44c944b0c51745
piyushAgrawal44 <piyushbansal941@gmail.com>
2026-03-10 23:38:16 +0530

    fix: solve invalid 'CD'  printing issue and adds global variables for folder paths

--- cdtool.bat
+++ cdtool.bat
@@ -1,24 +1,28 @@
 @echo off
 
-if "%1"=="show-all" (
+:: Define project paths
+set PERSONAL_PATH=C:\piyush-work\personal
+set PORTFOLIO_PATH=C:\piyush-work\personal\my-portfolio
+
+if "%1"=="" (
     echo Usage: cdtool [alias]
     echo.
     echo Available aliases:
-    echo   personal   -^>  C:\piyush-work\personal
-    echo   company   -^>  C:\piyush-work\company
+    echo personal     -^> %PERSONAL_PATH%
+    echo portfolio -^> %PORTFOLIO_PATH%
     goto :eof
 )
 
 
 if /i "%1"=="personal" (
-    cd /d "C:\piyush-work\personal"
-    echo Switched to: %CD%
+    cd /d "%PERSONAL_PATH%"
+    echo Switched to: %PERSONAL_PATH%
     goto :eof
 )
 
-if /i "%1"=="company" (
-    cd /d "C:\piyush-work\company\projects"
-    echo Switched to: %CD%
+if /i "%1"=="portfolio" (
+    cd /d "%PORTFOLIO_PATH%"
+    echo Switched to: %PORTFOLIO_PATH%
     goto :eof
 )
 
